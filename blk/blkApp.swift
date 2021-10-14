09d7f1e0fc6580dc37b388fa8023ab26a08f0058
Carlos Cabanero <carlos@blink.sh>
2021-10-14 18:05:25 -0400

    Initial Commit

---
+++ blk/blkApp.swift
@@ -0,0 +1,17 @@
+//
+//  blkApp.swift
+//  blk
+//
+//  Created by Carlos Cabanero on 10/14/21.
+//
+
+import SwiftUI
+
+@main
+struct blkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
